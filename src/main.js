6dbfa563649fbcf991c746eabba4a20a84b5ad05
JaxBBLL <showcye@foxmail.com>
2023-12-08 14:17:00 +0800

    style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'file-icons-js/css/style.css'
-import './style/index.css'
+import './style/index.less'
 
 const app = createApp(App)
 
